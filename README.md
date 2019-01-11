753498ae393899792f84ad5189e66d9493751b33
John Wooden <john.wooden@careerbuilder.com>
2019-01-10 23:55:01 -0500

    Add support for GCP Storage trigger

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # image-resize
+
+Supports GCP Cloud Storage bucket trigger (Finalize/Create).
+Generates thumbnail and uploads to a folder within same bucket. Simple check prevents thumbnail uploads from creating an infinite loop.
+
+## Example:
+
+Upload: xxx/yyy.zzz
+Thumbnail saved to: thumbnails/xxx/yyy.zzz
+
+## Original Author
+
 Code for blog post
  https://maartenvanvliet.nl/2017/07/29/dynamic-image-resizing-with-google-cloud-function/
+ 
--- index.js
+++ index.js
@@ -1,36 +1,61 @@
 //index.js
 
+/**
+ * Triggered from a change to a Cloud Storage bucket.
+ *
+ * @param {!Object} event Event payload and metadata.
+ * @param {!Function} callback Callback function to signal completion.
+ */
 const storage = require('@google-cloud/storage')();
 const gm = require('gm').subClass({imageMagick: true});
 
 const bucket = "source-bucket.example.com";
+const destPrefix = "thumbnails/";
 const width = 50
 const height = 50
-const option = "!"
-const quality = 90
+const option = "^"
+const quality = 75
 
-exports.main = (req, res) => {
-  const filename = req.query.f
-
-  if(!filename) {
-    res.sendStatus(500).send("No file specified");;
+exports.main = (event, callback) => {
+  const filename = event.data.name;
+ 
+ if(!filename) {
+    callback();
     return;
   }
-
+  
+  console.log(`Processing file: ${filename}`);
+  
+  if(filename.indexOf(destPrefix) > -1) {
+    callback();
+    return;
+  }
+  
   const file = storage.bucket(bucket).file(filename);
 
   let stream = file.createReadStream()
 
-  stream.on('error', function(err) {
-    console.error(err);
-    res.sendStatus(err.code).end(err);
-  });
+  const thumbFileName = `${destPrefix}${filename}`;
+  console.log(`Destination: ${thumbFileName}`);
+  
+  const dest = storage.bucket(bucket).file(thumbFileName);
+  let thumbnailUploadStream = dest.createWriteStream();
+  
 
   gm(stream)
+    .autoOrient()
     .resize(width, height, option)
     .quality(quality)
     .stream()
-    .pipe(res);
+    .pipe(thumbnailUploadStream);
 
-  console.log('Served: ' + filename );
-}
+  const streamAsPromise = new Promise((resolve, reject) =>
+    thumbnailUploadStream.on('finish', resolve).on('error', reject)
+);
+  
+  streamAsPromise.then(() => {
+    console.log('Served: ' + filename );
+    callback();
+  });
+
+};
